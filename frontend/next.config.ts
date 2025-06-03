e6d977e319611124b2f854a455e29668e8c88118
eyesilk <danila.stepanov2007@gmail.com>
2025-06-03 17:32:44 +0300

    Trying to fix a deply bug

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -16,14 +16,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.SERVER_URL}/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: `${process.env.SERVER_URL}/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
